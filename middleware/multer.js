87b4166ef30fbc6905af852a4a93d3efaf794b34
Mathieu Favraud <mathieu.favraud@gmail.com>
2023-09-18 14:40:08 +0200

    minor corrections to do

--- middleware/multer.js
+++ middleware/multer.js
@@ -14,12 +14,10 @@ const storage = multer.diskStorage({
     const name_extension = file.originalname.split(" ").join("_");
     const name = name_extension.split(".");
     const extension = MIME_TYPES[file.mimetype];
-    console.log(Date.now());
-    console.log(name[0] + Date.now() + "." + extension);
     callback(null, name[0] + Date.now() + "." + extension);
   },
 });
 
 module.exports = multer({ storage: storage }).single("image");
 
-//ajout de la date ne marche pas toujours ?
+// pourquoi pas de next() ?
--- middleware/sharp.js
+++ middleware/sharp.js
@@ -4,19 +4,27 @@ const fs = require("fs");
 const compression = (req, res, next) => {
   const input = `images/${req.file.filename}`;
   const output = `images/${req.file.filename}.webp`;
+
+  console.log(input);
+
   sharp(input)
     .webp()
     .toFile(output)
     .then((info) => console.log("conversion reussie " + info))
     .catch((error) => console.log(error));
+  req.file.path = output;
+  req.file.filename += ".webp";
+  /*
   fs.unlink(input, (error) => {
     if (error) {
       console.log(error);
     }
   });
-  req.file.path = output;
-  req.file.filename += ".webp";
+  */
   next();
 };
 
 module.exports = compression;
+
+//verification avant le fs unlink ?
+//suppression en asychrone ?
